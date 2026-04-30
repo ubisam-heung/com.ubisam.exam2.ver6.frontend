6a9e7d9cf736f3157272c9cfc5a2a16b3e6fdaf4
조흥재 연구원 <heung@ubisam.com>
2026-04-30 15:51:26 +0900

    20260430 contents/userAccount.vue 생성

---
+++ src/apps/02_bus/pages/contents/userAccount.vue
@@ -0,0 +1,441 @@
+<template>
+  <v-container class="pa-4" fluid>
+    <v-card>
+      <v-card-title class="d-flex align-center pe-2">
+        <v-icon icon="mdi-video-input-component"></v-icon> &nbsp;
+        {{ $t("frontend.contents.account.title") }}&nbsp;
+        <!-- 
+        //////////////////////////
+        // Search Field Start
+        //////////////////////////
+        -->
+        <v-text-field
+          class="ms-10"
+          v-model="searchForm.username"
+          density="compact"
+          label="Search"
+          prepend-inner-icon="mdi-magnify"
+          variant="solo-filled"
+          flat
+          hide-details
+          single-line
+        ></v-text-field>
+        <!-- 
+        //////////////////////////
+        // Search Field End
+        //////////////////////////
+        -->
+      </v-card-title>
+
+      <v-divider></v-divider>
+
+      <v-data-table-server
+        fixed-header
+        density="compact"
+        :loading="loading"
+        :search="config.searchBy"
+        :page="1"
+        :items-per-page="20"
+        :sort-by="config.sortBy"
+        :items-per-page-options="config.itemsPerPageOptions"
+        :headers="config.headers"
+        :item-value="config.itemValue"
+        :items-length="entitiesTotal"
+        :items="entities"
+        @update:options="searchAction"
+      >
+        <!-- 
+        //////////////////////////
+        # Table Cell Template Start
+        //////////////////////////
+        -->
+        <template v-slot:item.id="{ item }">
+          <v-btn
+            variant="plain"
+            color="primary"
+            :text="item.id"
+            :disabled="userinfo.subject != item.id"
+            style="text-transform: none"
+            @click="readAction(item)"
+          ></v-btn>
+        </template>
+
+        <template v-slot:item.updated.timestamp="{ item }">
+          {{ $moment.format(item.updated.timestamp) }}
+        </template>
+        <!-- 
+        //////////////////////////
+        # Table Cell Template End
+        //////////////////////////
+        -->
+
+        <template v-slot:footer.prepend>
+          <v-btn class="ms-1" text variant="elevated" @click="refreshAction">
+            <v-icon>mdi-refresh</v-icon>
+          </v-btn>
+
+          <v-spacer></v-spacer>
+        </template>
+      </v-data-table-server>
+
+      <v-dialog v-model="dialog" persistent width="800">
+        <v-card
+          prepend-icon="mdi-update"
+          :title="$t('frontend.contents.account.title')"
+          :subtitle="editForm.username"
+        >
+          <v-card-text>
+            <v-form validate-on="eager" @update:model-value="dialogValidate">
+              <!-- 
+              //////////////////////////
+              # Edit Form Start
+              //////////////////////////
+              -->
+              <v-text-field
+                v-model="editForm.username"
+                :rules="[$rules.requried]"
+                label="username"
+                placeholder="username"
+                hint="......."
+                variant="outlined"
+              ></v-text-field>
+              <!-- 
+              //////////////////////////
+              # Edit Form End
+              //////////////////////////
+              -->
+            </v-form>
+          </v-card-text>
+
+          <v-card-actions>
+            <v-btn
+              class="ms-5"
+              variant="elevated"
+              color="primary"
+              text="Save"
+              :disabled="!validate"
+              @click="isNew ? createAction(e) : updateAction(e)"
+            ></v-btn>
+            <v-btn text="Cancel" @click="cancelAction"></v-btn>
+
+            <v-spacer></v-spacer>
+            <v-btn
+              v-if="false"
+              color="error"
+              text="Delete"
+              variant="text"
+              @click="deleteAction"
+            ></v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </v-card>
+  </v-container>
+</template>
+
+
+
+<script>
+const x = "[/contents/userAccounts]";
+import $busServer from "@@/assets/apis/bus-server.js";
+import $common from "@@/assets/stores/common";
+
+export default {
+  data: () => ({
+    loading: false,
+    dialog: false,
+    isNew: false,
+    validate: false,
+
+    searchForm: {},
+    editForm: {},
+
+    entities: [],
+    entitiesTotal: 0,
+
+    config: {
+      searchBy: "",
+      itemsPerPageOptions: [
+        { value: 10, title: "10" },
+        { value: 20, title: "20" },
+        { value: 50, title: "50" },
+        { value: 100, title: "100" },
+      ],
+
+      ///////////////////////////////
+      // Config Start
+      ///////////////////////////////
+      itemValue: "username",
+
+      headers: [
+        { key: "id", title: "id", align: "start" },
+        { key: "username", title: "username", align: "start" },
+        { key: "provider", title: "provider", align: "end" },
+        { key: "updated.timestamp", title: "updatedTimestamp", align: "end" },
+      ],
+      sortBy: [
+        {key: "updated.timestamp", order: "desc" }
+      ],
+
+      initForm: {
+        
+      }
+      /////////////////////////////////
+      // Config End
+      /////////////////////////////////
+    },
+  }),
+
+  watch: {
+    searchForm: {
+      handler(e) {
+        this.refreshAction();
+      },
+      deep: true,
+    },
+  },
+
+  computed: {
+    subtitle: $common.computed.subtitle,
+    userinfo : $common.computed.userinfo,
+  },
+
+  methods: {
+
+    ////////////////////////////////////////
+    // handle....
+    ////////////////////////////////////////
+    handleCreate(){
+      return $busServer.accounts.create(this.editForm);
+    },
+    handleRead(entity){
+      return $busServer.accounts.read(entity);
+    },
+    handleUpdate(){
+      return $busServer.accounts.update(this.editForm);
+    },
+    handleDelete(){
+      return $busServer.accounts.delete(this.editForm);
+    },
+    handleSearch(query){
+      return $busServer.accounts.search(this.searchForm, query);
+    },
+    handleEntities(res){
+      this.entitiesTotal = res.page.totalElements;
+      this.entities = res._embedded.accounts;
+      return res;
+    },
+    handleEntity(res){
+      this.editForm = res ? res : Object.assign({}, this.config.initForm);
+      return res;
+    },
+
+    ////////////////////////////////////////
+    //
+    ////////////////////////////////////////
+    dialogOpen(isNew) {
+      this.dialog = true;
+      this.isNew = isNew;
+      return "opened";
+    },
+    dialogClose() {
+      this.dialog = false;
+      this.isNew = false;
+      return "closed";
+    },
+    dialogValidate(r){
+      this.validate = r;
+    },
+
+    ////////////////////////////////////////
+    //
+    ////////////////////////////////////////
+    confirmBefore(code) {
+      let msg = this.$t(`$dialog.before.${code}`);
+      return this.$dialog.confirm(msg);
+    },
+    confirmAfter(code) {
+      let msg = this.$t(`$dialog.after.${code}`);
+      return this.$dialog.alert(msg);
+    },
+    confirmError(code) {
+      let msg = this.$t(`$dialog.error.${code}`);
+      return this.$dialog.alert(msg, code);
+    },
+
+    ////////////////////////////////////////
+    //
+    ////////////////////////////////////////    
+    actionStart(loading) {
+      this.loading = true == loading ? true : false;
+      return Promise.resolve();
+    },
+    actionEnd(refresh) {
+      this.loading = false;
+      if (true == refresh) {
+        this.dialog = false;
+        this.isNew = false;
+        this.refreshAction();
+      }
+    },
+
+    ////////////////////////////////////////
+    //
+    ////////////////////////////////////////
+    refreshAction() {
+      this.config.searchBy = String(Date.now());
+    },
+
+    searchAction(params) {
+      this.actionStart(true)
+        .then((r) => {
+          return this.handleSearch(params);
+        })
+        .then((r) => {
+          return this.handleEntities(r);
+        })
+        .then((r) => {
+          return this.actionEnd(false);
+        })
+        .catch((e) => {
+          return this.confirmError(e);
+        })
+        .catch((e) => {
+          console.log(x, "searchAction", 2, e);
+          this.$router.push("/contents");
+        });
+    },
+
+    newAction() {
+      this.actionStart(true)
+        .then((r) => {
+          return this.handleEntity();
+        })
+        .then((r) => {
+          return this.dialogOpen(true);
+        })
+        .then((r) => {
+          return this.actionEnd(false);
+        });
+    },
+
+    cancelAction() {
+      this.actionStart(true)
+        .then((r) => {
+          return this.dialogClose();
+        })
+        .then((r) => {
+          return this.handleEntity();
+        })
+        .then((r) => {
+          return this.actionEnd(false);
+        });
+    },
+
+    createAction() {
+      this.confirmBefore("create")
+        .then((r) => {
+          return this.actionStart(true);
+        })
+        .then((r) => {
+          return this.handleCreate();
+        })
+        .then((r) => {
+          return this.confirmAfter("create");
+        })
+        .then((r) => {
+          return this.handleEntity();
+        })
+        .then((r) => {
+          return this.actionEnd(true);
+        })
+        .catch((r) => {
+          return this.confirmError(r);
+        })
+        .catch((r) => {
+          return this.actionEnd(true);
+        });
+    },
+
+    readAction(entity) {
+      this.actionStart(true)
+        .then((r) => {
+          return this.handleRead(entity);
+        })
+        .then((r) => {
+          return this.handleEntity(r);
+        })
+        .then((e) => {
+          return this.dialogOpen(false);
+        })
+        .then((e) => {
+          return this.actionEnd(false);
+        })
+        .catch((e) => {
+          return this.confirmError(e);
+        })
+        .catch((e) => {
+          return this.actionEnd(true);
+        });
+    },
+
+    updateAction() {
+      this.confirmBefore("update")
+        .then((r) => {
+          return this.actionStart(true);
+        })
+        .then((r) => {
+          return this.handleUpdate();
+        })
+        .then((r) => {
+          console.log(1);
+          return this.confirmAfter("update");
+        })
+        .then((r) => {
+          console.log(2);
+          return this.handleEntity();
+        })
+        .then((r) => {
+          console.log(3);
+          return this.actionEnd(true);
+        })
+        .catch((r) => {
+          console.log(4);
+          return this.confirmError(r);
+        })
+        .catch((r) => {
+          return this.actionEnd(true);
+        });
+    },
+
+    deleteAction() {
+      this.confirmBefore("delete")
+        .then((r) => {
+          return this.actionStart(true);
+        })
+        .then((r) => {
+          return this.handleDelete();
+        })
+        .then((r) => {
+          return this.confirmAfter("delete");
+        })
+        .then((r) => {
+          return this.handleEntity();
+        })
+        .then((r) => {
+          return this.actionEnd(true);
+        })
+        .catch((r) => {
+          return this.confirmError(r);
+        })
+        .catch((r) => {
+          return this.actionEnd(true);
+        });
+    },
+  },
+
+  mounted() {
+    this.subtitle = x;
+  },
+};
+</script>
